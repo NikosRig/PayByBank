5ed0c30bba999c7fafec741aea73bd4e8491e595
NikosRig <nikosrigas95@gmail.com>
2022-03-30 00:58:33 +0300

    CreatePaymentOrderValidatorBuilder added

---
+++ src/WebApi/Actions/PaymentOrders/CreatePaymentOrder/CreatePaymentOrderValidatorBuilder.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayByBank\WebApi\Actions\PaymentOrders\CreatePaymentOrder;
+
+use PayByBank\WebApi\Modules\Validation\RequestParamValidator;
+use PayByBank\WebApi\Modules\Validation\Rules\AmountRule;
+use PayByBank\WebApi\Modules\Validation\Rules\BankRule;
+use PayByBank\WebApi\Modules\Validation\Rules\CreditorIbanRule;
+use PayByBank\WebApi\Modules\Validation\Rules\CreditorNameRule;
+use PayByBank\WebApi\Modules\Validation\Validator;
+use PayByBank\WebApi\Modules\Validation\ValidatorBuilder;
+
+class CreatePaymentOrderValidatorBuilder implements ValidatorBuilder
+{
+    /**
+     * @return Validator
+     */
+    public function build(): Validator
+    {
+        return (new RequestParamValidator())
+            ->withRule(new CreditorIbanRule())
+            ->withRule(new CreditorNameRule())
+            ->withRule(new AmountRule())
+            ->withRule(new BankRule());
+    }
+}

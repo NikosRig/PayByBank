82e7845e1f222f11d944d73fc22ed35b3da497d7
NikosRig <nikosrigas95@gmail.com>
2022-06-03 18:39:43 +0300

    ValidatorBuilder docs updated

--- src/WebApi/Modules/Validation/ValidatorBuilder.php
+++ src/WebApi/Modules/Validation/ValidatorBuilder.php
@@ -4,7 +4,13 @@ declare(strict_types=1);
 
 namespace PayByBank\WebApi\Modules\Validation;
 
+use InvalidArgumentException;
+
 interface ValidatorBuilder
 {
+    /**
+     * @throws InvalidArgumentException
+     * @return Validator
+     */
     public function build(): Validator;
 }

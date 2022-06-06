caf4efd4d501d13efa6664f8bde871e38d1643fc
NikosRig <nikosrigas95@gmail.com>
2022-06-07 01:17:40 +0300

    Create Bank Account integration test added

---
+++ test/Integration/WebApi/Actions/CreateBankAccount/CreateBankAccountTest.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Test\Integration\WebApi\Actions\CreateBankAccount;
+
+use GuzzleHttp\Psr7\Request;
+use Psr\Http\Client\ClientExceptionInterface;
+use Test\Integration\IntegrationTestCase;
+
+class CreateBankAccountTest extends IntegrationTestCase
+{
+    /**
+     * @throws ClientExceptionInterface
+     */
+    public function testShouldCreateBankAccount(): void
+    {
+        $mid = $this->createMerchant();
+        $body = json_encode([
+            'mid' => $mid,
+            'iban' => $this->faker->iban(),
+            'accountHolderName' => $this->faker->name()
+        ]);
+
+        $request = new Request(
+            'PUT',
+            "http://{$_ENV['WEB_API_HOST']}/merchant/accounts",
+            [],
+            $body
+        );
+        $response = $this->client->sendRequest($request);
+        $this->assertEquals(201, $response->getStatusCode());
+    }
+}

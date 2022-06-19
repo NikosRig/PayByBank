dc157fd8611f50fefecf7ca27f32da3f378428f1
NikosRig <nikosrigas95@gmail.com>
2022-06-20 01:22:04 +0300

    ActionsTestCase mockServerRequest accepts headers

--- test/Unit/WebApi/Actions/ActionTestCase.php
+++ test/Unit/WebApi/Actions/ActionTestCase.php
@@ -10,9 +10,10 @@ use Psr\Http\Message\StreamInterface;
 
 class ActionTestCase extends TestCase
 {
-    public function mockServerRequest(string $body = ''): ServerRequestInterface
+    public function mockServerRequest(string $body = '', array $headers = []): ServerRequestInterface
     {
         $serverRequest = $this->createMock(ServerRequestInterface::class);
+        $serverRequest->method('getHeaders')->willReturn($headers);
         $stream = $this->createMock(StreamInterface::class);
         $stream->method('getContents')->willReturn($body);
         $serverRequest->method('getBody')->willReturn($stream);

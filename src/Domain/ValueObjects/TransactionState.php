f4f0aeba8e44a3572865a5321abde40a3cdb8bcd
NikosRig <nikosrigas95@gmail.com>
2022-07-26 01:06:25 +0300

    Transaction state created

---
+++ src/Domain/ValueObjects/TransactionState.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PayByBank\Domain\ValueObjects;
+
+use DateTime;
+
+class TransactionState
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly DateTime $dateCreated,
+        public readonly string $bankAccountId,
+        public readonly string $paymentOrderToken,
+        public readonly string $scaRedirectUrl,
+        public readonly string $transactionId,
+        public readonly string $psuIp
+    ) {
+    }
+}
